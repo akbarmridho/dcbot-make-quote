1cdb189fcdda2b1a37d8f7c2909211e1d5d43133
Akbar Maulana R <akbarmaulanaridho1@gmail.com>
2022-06-09 09:39:58 +0700

    config only for admin

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -1,4 +1,5 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
+import { Permissions } from 'discord.js'
 import { getConfig } from '../database/models/config'
 import { Command, CommandError } from '../interfaces/command'
 
@@ -8,11 +9,18 @@ export const config: Command = {
     .setDescription('Configure channel target to send quotes.')
     .addBooleanOption(option => option.setName('same-channel')
       .setRequired(true)
-      .setDescription('Jika True, hasil quote akan selalu dikirim ke channel ini')),
+      .setDescription('Jika True, hasil quote akan selalu dikirim ke channel ini.')),
   async run (interaction) {
     await interaction.deferReply()
 
     if (!interaction.inGuild()) throw new CommandError('Not in guild')
+
+    const member = await interaction.guild!.members.fetch(interaction.user!.id)
+
+    if (!member.permissions.has(Permissions.FLAGS.ADMINISTRATOR)) {
+      await interaction.editReply({ content: 'You need administrator permission to be able to change this settings.' })
+      return
+    }
 
     const serverConfig = await getConfig(interaction.guildId)
 
