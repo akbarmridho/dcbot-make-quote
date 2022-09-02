435672f416ff73efa93fd5b514ba46b345f35428
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2022-09-02 20:55:16 +0700

    add user config model and command

---
+++ src/commands/config-user.ts
@@ -0,0 +1,80 @@
+import { PermissionsBitField, SlashCommandBuilder } from 'discord.js'
+import { createUserConfig } from '../database/models/user'
+import { Command, CommandError } from '../interfaces/command'
+
+export const configQuote: Command = {
+  data: new SlashCommandBuilder()
+    .setName('config-user')
+    .setDescription("Set user's quote configuration")
+    .addStringOption((option) =>
+      option
+        .setName('profile-image')
+        .setRequired(true)
+        .addChoices(
+          {
+            name: 'Server Profile',
+            value: 'server'
+          },
+          {
+            name: 'Default Profile',
+            value: 'default'
+          }
+        )
+        .setDescription('Atur gambar profil pengguna yang ditampilkan')
+    )
+    .addStringOption((option) =>
+      option
+        .setName('profile-name')
+        .setRequired(true)
+        .addChoices(
+          {
+            name: 'Server Profile',
+            value: 'server'
+          },
+          {
+            name: 'Default Profile',
+            value: 'default'
+          }
+        )
+        .setDescription('Atur nama profil pengguna yang ditampilkan')
+    ),
+  async run(interaction) {
+    await interaction.deferReply()
+
+    if (!interaction.inGuild()) throw new CommandError('Not in guild')
+
+    const member = await interaction.guild!.members.fetch(interaction.user!.id)
+
+    if (!member.permissions.has(PermissionsBitField.Flags.Administrator)) {
+      await interaction.editReply({
+        content:
+          'You need administrator permission to be able to change this settings.'
+      })
+      return
+    }
+
+    const profileImage = interaction.options.getString('profile-image', true)
+    const profileName = interaction.options.getString('profile-name', true)
+
+    if (
+      !(profileImage === 'default' || profileImage === 'server') ||
+      !(profileName === 'default' || profileName === 'server')
+    ) {
+      throw Error('Invalid config option')
+    }
+
+    const userConfig = await createUserConfig(
+      interaction.user.id,
+      interaction.guildId,
+      profileImage,
+      profileName
+    )
+
+    if (!userConfig) {
+      throw Error('User model null')
+    }
+
+    await userConfig.save()
+    await interaction.editReply('Perubahan berhasil disimpan.')
+  }
+}
---
+++ src/database/models/user.ts
@@ -0,0 +1,52 @@
+import { model, Schema } from 'mongoose'
+
+type Options = 'default' | 'server'
+
+export interface UserConfigInterface {
+  userId: string
+  serverId: string
+  profileImage: Options
+  authorTag: Options
+}
+
+export const userConfigSchema = new Schema<UserConfigInterface>({
+  serverId: { type: String, required: true },
+  userId: { type: String, required: true },
+  profileImage: { type: String, default: 'default' },
+  authorTag: { type: String, default: 'default' }
+})
+
+export const userConfigModel = model<UserConfigInterface>(
+  'UserConfig',
+  userConfigSchema
+)
+
+export const getuserConfig = async (userId: string, serverId: string) => {
+  return await userConfigModel.findOne({
+    userId,
+    serverId
+  })
+}
+
+export const createUserConfig = async (
+  userId: string,
+  serverId: string,
+  profileImage: Options,
+  authorTag: Options
+) => {
+  return await userConfigModel.findOneAndUpdate(
+    {
+      userId,
+      serverId
+    },
+    {
+      profileImage,
+      authorTag,
+      userId,
+      serverId
+    },
+    {
+      upsert: true
+    }
+  )
+}
